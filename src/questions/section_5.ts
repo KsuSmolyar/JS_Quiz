d66bff35fe3595c4c3ea8d99de54385acb58f348
@KsuSmolyar <ibragimova-ksenija@lenta.ru>
2023-09-07 16:30:22 +0400

    add new QuizSection

---
+++ src/questions/section_5.ts
@@ -0,0 +1,484 @@
+import { QuestionCard } from "./types";
+//Числа
+export const questionCards_4: QuestionCard[] = [
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.1.1",
+        name: "q5.1",
+        value: "1",
+        text: "текст",
+      },
+      {
+        id: "5.1.2",
+        name: "q5.1",
+        value: "0",
+        text: "текст",
+      },
+      {
+        id: "5.1.3",
+        name: "q5.1",
+        value: "0",
+        text: "текст",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.2.1",
+        name: "q5.2",
+        value: "1",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.2.2",
+        name: "q5.2",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.2.3",
+        name: "q5.2",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.3.1",
+        name: "q5.3",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.3.2",
+        name: "q5.3",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.3.3",
+        name: "q5.3",
+        value: "1",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.4.1",
+        name: "q5.4",
+        value: "1",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.4.2",
+        name: "q5.4",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.4.3",
+        name: "q5.4",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.5.1",
+        name: "q5.5",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.5.2",
+        name: "q5.5",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.5.3",
+        name: "q5.5",
+        value: "1",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.6.1",
+        name: "q5.6",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.6.2",
+        name: "q5.6",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.6.3",
+        name: "q5.6",
+        value: "1",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.7.1",
+        name: "q5.7",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.7.2",
+        name: "q5.7",
+        value: "1",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.7.3",
+        name: "q5.7",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.8.1",
+        name: "q5.8",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.8.2",
+        name: "q5.8",
+        value: "1",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.8.3",
+        name: "q5.8",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.9.1",
+        name: "q5.9",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.9.2",
+        name: "q5.9",
+        value: "1",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.9.3",
+        name: "q5.9",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.10.1",
+        name: "q5.10",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.10.2",
+        name: "q5.10",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.10.3",
+        name: "q5.10",
+        value: "1",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.11.1",
+        name: "q5.11",
+        value: "1",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.11.2",
+        name: "q5.11",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.11.3",
+        name: "q5.11",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.12.1",
+        name: "q5.12",
+        value: "1",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.12.2",
+        name: "q5.12",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.12.3",
+        name: "q5.12",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.13.1",
+        name: "q5.13",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.13.2",
+        name: "q5.13",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.13.3",
+        name: "q5.13",
+        value: "1",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.14.1",
+        name: "q5.14",
+        value: "1",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.14.2",
+        name: "q5.14",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.14.3",
+        name: "q5.14",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.15.1",
+        name: "q5.15",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.15.2",
+        name: "q5.15",
+        value: "1",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.15.3",
+        name: "q5.15",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.16.1",
+        name: "q5.16",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.16.2",
+        name: "q5.16",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.16.3",
+        name: "q5.16",
+        value: "1",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.17.1",
+        name: "q5.17",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.17.2",
+        name: "q5.17",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.17.3",
+        name: "q5.17",
+        value: "1",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.18.1",
+        name: "q5.18",
+        value: "0",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.18.2",
+        name: "q5.18",
+        value: "1",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.18.3",
+        name: "q5.18",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.19.1",
+        name: "q5.19",
+        value: "1",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.19.2",
+        name: "q5.19",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.19.3",
+        name: "q5.19",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+  {
+    questionText: "Текст вопроса",
+    answerOptions: [
+      {
+        id: "5.20.1",
+        name: "q5.20",
+        value: "1",
+        text: "текст ответа номер 1",
+      },
+      {
+        id: "5.20.2",
+        name: "q5.20",
+        value: "0",
+        text: "текст ответа  номер 2",
+      },
+      {
+        id: "5.20.3",
+        name: "q5.20",
+        value: "0",
+        text: "текст ответа номер 3",
+      }
+    ],
+    feedbackText: "Текст обратной связи"
+  },
+]
